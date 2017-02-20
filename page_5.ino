9f4b2499e99598db7810478ebf1b76b07921b29c
as2pic <qalinta@gmail.com>
2017-02-20 18:32:15 +0100

    Add files via upload

---
+++ page_5.ino
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                          Page réglage Hors gel
+/////////////////////////////////////////////////////////////////////////////////
+void P5_b2PopCallback(void *ptr) // Bouton retour
+{
+    page3.show();
+    num_page = 3;
+    dtostrf(conshorsgel, 5, 1, buffer);
+    P3_t2.setText(buffer);
+}
+
+void P5_b0PopCallback(void *ptr) // Bouton "-"
+{
+    conshorsgel -= 0.1;
+    dtostrf(conshorsgel, 5, 1, buffer);
+    P5_t1.setText(buffer);
+    EEPROM.writeDouble(addr_conshorsgel, conshorsgel);
+}
+
+void P5_b1PopCallback(void *ptr) // Bouton "+"
+{
+    conshorsgel += 0.1;
+    dtostrf(conshorsgel, 5, 1, buffer);
+    P5_t1.setText(buffer);
+    EEPROM.writeDouble(addr_conshorsgel, conshorsgel);
+}
+
+
+
---
+++ z_info_version.ino
@@ -0,0 +1,13 @@
+//************************************************
+//*            Programme domogarden              *
+//************************************************
+// V1.0 -> 1ere version fonctionnelle
+// V2.0 -> Modification de la partie etalonnage pH (2 points)
+// V2.1 -> Ajout gestion arrosage intégré
+//************************************************
+
+// a faire:
+// -> page WEB
+// optimiser stockage eeprom
+// optimiser déclaration valeur (type)
+
